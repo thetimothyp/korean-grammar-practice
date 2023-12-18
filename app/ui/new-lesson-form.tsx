6415b6740428322ba9b52cd8f07343410b6ea727
thetimothyp <thetimothyp@gmail.com>
2023-12-18 12:16:00 -0800

    simple validation for new lesson form

--- app/ui/new-lesson-form.tsx
+++ app/ui/new-lesson-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ForwardRefEditor } from "./MDXEditor/ForwardRefEditor";
 import { type MDXEditorMethods } from "@mdxeditor/editor";
 import '@mdxeditor/editor/style.css';
@@ -8,34 +8,80 @@ import '@mdxeditor/editor/style.css';
 export default function NewLessonForm({ uid }: { uid: string }) {
   const [title, setTitle] = useState('');
   const [summary, setSummary] = useState('');
+  const [body, setBody] = useState('');
+
+  const [didSubmit, setDidSubmit] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+  const [summaryError, setSummaryError] = useState(false);
+  const [bodyError, setBodyError] = useState(false);
+
   const ref = useRef<MDXEditorMethods>(null);
 
-  function handleSubmit(e: any) {
-    e.preventDefault();
-    
-    const req = async () => {
-      const response = await fetch('/api/exercise/new', {
-        method: 'POST',
-        body: JSON.stringify({ title, summary })
-      });
-      return response.json();
-    };
-    req().then(() => { alert('Success!'); });
+  function validate() {
+    if (didSubmit) {
+      if (title.length == 0) {
+        setTitleError(true);
+      } else {
+        setTitleError(false);
+      }
+      if (summary.length == 0) {
+        setSummaryError(true);
+      } else {
+        setSummaryError(false);
+      }
+      if (body.length == 0) {
+        setBodyError(true);
+      } else {
+        setBodyError(false);
+      }
+    }
+  }
+
+  useEffect(validate, [title, summary, body, didSubmit]);
+
+  function isValid() {
+    return title.length > 0 && summary.length > 0 && body.length > 0;
+  }
+
+  function handleSubmit() {
+    setDidSubmit(true);
+    if (isValid()) {
+      // const req = async () => {
+      //   const response = await fetch('/api/exercise/new', {
+      //     method: 'POST',
+      //     body: JSON.stringify({ title, summary })
+      //   });
+      //   return response.json();
+      // };
+      // req().then(() => { alert('Success!'); });
+    }
   }
 
   return (
     <div className="flex flex-col items-center min-h-screen px-4 w-full md:w-4/5 xl:w-3/5">
       <div className='flex w-full justify-between items-center border-b border-dashed py-4'>
         <div className='flex flex-col w-4/5 justify-left'>
-          <input name='title' value={title} onChange={e => setTitle(e.target.value)} className="bg-transparent px-1 text-3xl focus:outline-none font-bold" placeholder="Untitled"></input>
-          <input name='summary' onChange={e => setSummary(e.target.value)} className="bg-transparent px-1 text-lg focus:outline-none" placeholder="Enter lesson summary..."></input>
+          <input
+            name='title'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            className={`${titleError ? 'bg-red-100' : ''} mb-1 rounded-md bg-transparent px-1 text-3xl focus:outline-none font-bold transition-colors`}
+            placeholder="Untitled" />
+          <input
+            name='summary'
+            onChange={e => setSummary(e.target.value)}
+            className={`${summaryError ? 'bg-red-100' : ''} rounded-md bg-transparent px-1 text-lg focus:outline-none transition-colors`}
+            placeholder="Enter lesson summary..." />
         </div>
-        <button className="bg-green-500 hover:bg-green-600 shadow-sm p-2 px-6 rounded-lg transition-colors">
+        <button
+          onClick={handleSubmit}
+          className="bg-green-500 hover:bg-green-600 shadow-sm p-2 px-6 rounded-lg transition-colors"
+        >
           <span className="font-bold tracking-wide text-white text-center antialiased">Save</span>
         </button>
       </div>
-      <div className='w-full py-6'>
-        <ForwardRefEditor placeholder="Write something!" className="max-w-none prose" ref={ref} markdown='' />
+      <div className={`${bodyError ? 'bg-red-100' : ''} rounded-md w-full my-6 transition-colors`}>
+        <ForwardRefEditor placeholder="Write something!" className="max-w-none prose" ref={ref} markdown='' onChange={setBody} />
       </div>
     </div>
   )
