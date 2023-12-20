30eaa545d0f4971fa845331804b1a0a1e3f880ee
Tim Park <thetimothyp@gmail.com>
2023-12-20 14:45:30 -0800

    fix for tailwind not including shadow color classes

--- app/ui/grid-tiles/grid-tile-link.tsx
+++ app/ui/grid-tiles/grid-tile-link.tsx
@@ -4,6 +4,11 @@ export default function GridTileLink({ href, color, children } : { href: string,
   return (
     <Link
       href={href}
+      // Tailwind won't include custom class names in the build
+      // unless the class name is in the code in its entirety:
+      // https://stackoverflow.com/a/68020542 
+
+      // hover:shadow-green hover:shadow-yellow hover:shadow-purple
       className={`row-span-3 top-[6px] min-h-[12rem] relative hover:top-0 hover:shadow-${color} hover:cursor-pointer bg-stone-50 justify-center flex flex-col items-start py-4 px-6 border-2 border-stone-600 rounded-2xl transition-all`}>
       {children}
     </Link>
