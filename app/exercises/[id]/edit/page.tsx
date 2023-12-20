f7e42e277d3c7e95a19a438da892674666d3336b
thetimothyp <thetimothyp@gmail.com>
2023-12-19 16:20:40 -0800

    add ui to edit exercise

---
+++ app/exercises/[id]/edit/page.tsx
@@ -0,0 +1,38 @@
+import { fetchExercise } from "@/app/lib/data";
+import EditExerciseForm from "@/app/ui/edit-exercise-form";
+
+// https://stackoverflow.com/a/58692591
+function flattenLessons(exercise: any[]): any {
+  const arrayObj = exercise.reduce((obj, row) => {
+    if (row.eid in obj) {
+      obj[row.eid].lessons.push({
+        id: row.lid,
+        title: row.lesson_title,
+        summary: row.lesson_summary
+      });
+    } else {
+      obj[row.eid] = {
+        id: row.eid,
+        nl_text: row.nl_text,
+        tl_text: row.tl_text,
+        lessons: [{
+          id: row.lid,
+          title: row.lesson_title,
+          summary: row.lesson_summary
+        }]
+      };
+    }
+    return obj;
+  }, {});
+  return Object.values(arrayObj)[0];
+}
+
+export default async function EditExercise({ params }: { params: { id: string } }) {
+  const exercise = await fetchExercise(params.id);
+  const flattenedExercise = flattenLessons(exercise);
+  return (
+    <main className="h-screen w-screen flex justify-center items-center">
+      <EditExerciseForm id={flattenedExercise.id} initialNlText={flattenedExercise.nl_text} initialTlText={flattenedExercise.tl_text} />
+    </main>
+  )
+}
---
+++ app/ui/edit-exercise-form.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+type EditExerciseFormProps = {
+  id?: string;
+  initialTlText: string;
+  initialNlText: string;
+};
+
+export default function EditExerciseForm({ id, initialTlText, initialNlText } : EditExerciseFormProps) {
+  const [tlText, setTlText] = useState(initialTlText || '');
+  const [nlText, setNlText] = useState(initialNlText || '');
+  const router = useRouter();
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    
+    const req = async () => {
+      const response = await fetch('/api/exercises/update', {
+        method: 'POST',
+        body: JSON.stringify({ id, nl_text: nlText, tl_text: tlText })
+      });
+      return response.json();
+    };
+    req().then((res) => { router.push(`/exercises/${res.id}/view`) });
+  }
+
+  return (
+    <form className='flex flex-col bg-none w-4/5 lg:w-3/5 xl:w-2/5 justify-left gap-2 my-2' onSubmit={handleSubmit}>
+        <h1 className="text-xl">{!id ? 'Create a new exercise' : 'Edit exercise'}</h1>
+        <label htmlFor="tlText" className="text-sm text-zinc-400">Target language text</label>
+        <input
+          name='tlText'
+          value={tlText}
+          onChange={e => setTlText(e.target.value)}
+          className="p-4 rounded-lg mb-2 text-lg"
+          placeholder="Enter native language text" />
+        <label htmlFor="nlText" className="text-sm text-zinc-400">Native language text</label>
+        <input
+          name='nlText'
+          value={nlText}
+          onChange={e => setNlText(e.target.value)}
+          className="p-4 rounded-lg mb-4 text-lg"
+          placeholder="Enter target language text" />
+        <button className="bg-green-500 hover:bg-green-600 lg:w-1/5 self-end shadow-sm p-2 px-4 rounded-lg transition-colors">
+          <span className="font-bold tracking-wide text-white text-center antialiased">Save</span>
+        </button>
+    </form>
+  )
+}
