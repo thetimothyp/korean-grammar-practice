487fdd1082c9a3bed7638e70a686378d9d971caa
thetimothyp <thetimothyp@gmail.com>
2023-12-24 11:35:33 -0800

    style create exercise form and update copy to use side_a side_b instead of TL NL

--- app/ui/edit-exercise-form.tsx
+++ app/ui/edit-exercise-form.tsx
@@ -94,25 +94,25 @@ export default function EditExerciseForm({ id, initialSideAText, initialSideBTex
   }
 
   return (
-    <div className="flex flex-col w-full h-full justify-center items-center">
+    <div className="flex flex-col w-full h-full items-center mt-24">
       <form className='flex flex-col bg-none w-4/5 lg:w-3/5 xl:w-2/5 justify-left gap-2 my-2' onSubmit={handleSubmit}>
-          <h1 className="text-xl">{!id ? 'Create a new exercise' : 'Edit exercise'}</h1>
-          <label htmlFor="sideBText" className="text-sm text-zinc-400">Native language text</label>
-          <input
-            name='sideBText'
-            value={sideBText}
-            onChange={e => setSideBText(e.target.value)}
-            className={`${sideBTextError ? 'bg-red-100' : ''} p-4 rounded-md mb-4 text-lg border-zinc-400/60 hover:border-zinc-400/90 transition-colors border`}
-            placeholder="Enter target language text" />
-          <label htmlFor="sideAText" className="text-sm text-zinc-400">Target language text</label>
+          <h1 className="text-3xl font-bold mb-4">{!id ? 'Create a new exercise' : 'Edit exercise'}</h1>
+          <label htmlFor="sideAText" className="">Side A text</label>
           <input
             name='sideAText'
             value={sideAText}
             onChange={e => setSideAText(e.target.value)}
-            className={`${sideATextError ? 'bg-red-100' : ''} p-4 rounded-md mb-4 text-lg border-zinc-400/60 hover:border-zinc-400/90 transition-colors border`}
-            placeholder="Enter native language text" />
+            className={`${sideATextError ? 'bg-red-100' : ''} p-4 rounded-md mb-4 text-lg border-stone-800 border-2 transition-colors border`}
+            placeholder="Enter side A text" />
+          <label htmlFor="sideBText" className="">Side B text</label>
+          <input
+            name='sideBText'
+            value={sideBText}
+            onChange={e => setSideBText(e.target.value)}
+            className={`${sideBTextError ? 'bg-red-100' : ''} p-4 rounded-md mb-4 text-lg border-stone-800 border-2 transition-colors border`}
+            placeholder="Enter side B text" />
 
-          <label htmlFor="lessons" className="text-sm text-zinc-400">Which lesson concepts does this exercise practice?</label>
+          <label htmlFor="lessons" className="">Which lessons does this exercise practice?</label>
           <AsyncSelect
             isMulti 
             name='lessons'
@@ -121,13 +121,21 @@ export default function EditExerciseForm({ id, initialSideAText, initialSideBTex
             loadOptions={lessonOptionsPromise}
             defaultValue={defaultLessons}
             onChange={handleLessonSelect}
-            classNames={lessonTagsError ? { control: () => '!bg-red-100', container: () => '!bg-red-100' } : {}}
+            classNames={
+              lessonTagsError ? {
+                control: () => '!bg-red-100', container: () => '!bg-red-100'
+              } : 
+              { 
+                control: () => '!border-2 !border-stone-800',
+                menu: () => 'border-2 border-stone-800'
+              }
+            }
           />
-          <button className="bg-green-500 hover:bg-green-600 lg:w-1/5 self-end shadow-sm p-2 px-4 rounded-lg transition-colors relative mt-4">
+          <button className="bg-stone-50 hover:bg-stone-200 lg:w-1/5 self-end shadow-green border-2 border-stone-800 p-2 px-10 rounded-lg transition-colors relative mt-8">
             <div className={`${isLoading ? 'opacity-100' : 'opacity-0'} w-full h-full rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-inherit transition-opacity`}>
-              <div className="mt-2 animate-spin inline-block w-6 h-6 border-[2px] border-white border-opacity-70 border-t-transparent rounded-full" role="status" aria-label="loading" />
+              <div className="mt-[13px] animate-spin inline-block w-6 h-6 border-[2px] border-white border-opacity-70 border-t-transparent rounded-full" role="status" aria-label="loading" />
             </div>
-            <span className="font-bold tracking-wide text-white text-center antialiased">Save</span>
+            <span className="font-bold tracking-wide text-center antialiased">Save</span>
           </button>
       </form>
     </div>
